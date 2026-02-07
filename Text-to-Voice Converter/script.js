98728879b1824a3e6922c45d7910024a7df82cf3
saphalthedeveloper-cyber <saphalthedeveloper@gmail.com>
2026-02-07 09:52:20 +0545

    Fix voice selection in Text-to-Voice Converter

--- Text-to-Voice Converter/script.js
+++ Text-to-Voice Converter/script.js
@@ -8,14 +8,14 @@ let voiceSelect=document.querySelector('select')
         voices=window.speechSynthesis.getVoices();
         speech.voice=voices[0];
 
-        voices.forEach((voice,i)=>
-        (voiceSelect.options[i])= new Option(voice.name,i))
+    voices.forEach((voice,i)=>
+        (voiceSelect.add(speechSelect.value)))
     }
-
     voiceSelect.addEventListener('change',()=>{
         speech.voice=voices[voiceSelect.value]
     })
 document.querySelector('button').addEventListener('click',()=>{
     speech.text=document.querySelector('textarea').value
     window.speechSynthesis.speak(speech)
+
 })
